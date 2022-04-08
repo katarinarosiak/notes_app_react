6c8a5c64e4a5caf4c7057c11aa169dfffc079ba1
katarina <katarinarosiak@gmail.com>
2022-04-08 17:56:23 +0200

    CRUD operation added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Note from './components/Note'
+import Footer from './components/Footer'
+import noteService from './services/notes'
 
-function App() {
+
+
+
+const App = (props) => {
+	const [ notes, setNotes ] = useState([])
+	const [ newNote, setNewNote ] = useState('a new note...')
+	const [showAll, setShowAll] = useState(true)
+	const [ errorMessage, setErrorMessage ] = useState('Some error happend');
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+	const deleteNote = (event, id) => {
+		event.preventDefault()
+
+		noteService 
+			.remove(id)
+			.then(() => {
+				const newNotes = notes.filter(note => note.id !== id)
+				setNotes(newNotes)
+			})
+
+	}
+
+	const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+	const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+	const toggleImportantNotes = () => {
+		setShowAll(!showAll)
+		console.log(showAll);
+	}
+
+	const notesToShow = showAll
+	? notes
+	: notes.filter(note => note.important === true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div>
+			<h1>Notes</h1>
+			<div>
+        <button onClick={toggleImportantNotes}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+            <Note 
+							key={note.id} 
+							note={note}
+							toggleImportance={() => toggleImportanceOf(note.id)}
+							deleteNote={(event) => deleteNote(event, note.id)}
+							 />
+        )}
+      </ul>
+			<form onSubmit={addNote}>
+			<input value={newNote} 
+			 onChange={handleNoteChange}	
+			/>
+				<button type="submit">save</button>
+			</form>
+			<Footer />
+		</div>
   );
 }
 
---
+++ src/components/Note.js
@@ -0,0 +1,20 @@
+const Note = ({ note, toggleImportance, deleteNote }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
+
+		const NoteStyle = {
+			color: 'grey',
+			paddingTop: 5,
+			fontSize: 15,
+		}
+
+  return (
+    <li style={NoteStyle}>
+      {note.content} 
+      <button onClick={toggleImportance}>{label}</button>
+			<button onClick={deleteNote}>Delete</button>
+    </li>
+  )
+}
+
+export default Note
